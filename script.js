26e0a947b1219409c43f118517fd55c26d8e6164
jabrams513 <145389973+jabrams513@users.noreply.github.com>
2023-11-24 11:33:27 -0500

    prompt for user to select a password length with validations and followup questions for character usage

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Generate password function
-function generatePassword(){
-  let characterNums = prompt("How many characters would you like in your password?");
-  let text;
-  if (characterNums == null || CharacterNums == "") {
-    text = "Invalid number of characters!";
-  } else {
-    text = "I will generate a " + characterNums + "character password!";
+// Write password to the #password input
+function writePassword() {
+  let promptedLength;
+  let validLength = false;
+  while (!validLength) {
+    promptedLength = prompt("How many characters would you like in your password? You may have between 8 and 128 characters.");
+    if (promptedLength >= 8 && promptedLength <= 128) {
+      validLength = true;
+    } else if (promptLength === null) {
+      break;
+    }
   }
-  console.log(text)
+
+  if (validLength == true) {
+    let useLowercase = confirm("Use lowercase characters? 'Ok' for yes, 'Cancel' for no.");
+    let useUppercase = confirm("Use uppercase characters? 'Ok' for yes, 'Cancel' for no.");
+    let useNumerals = confirm("Use numerals characters? 'Ok' for yes, 'Cancel' for no.");
+    let useSpecial = confirm("Use special characters? 'Ok' for yes, 'Cancel' for no.");
+    var password = generatePassword(promptLength, useLowercase, useUppercase, useNumerals, useSpecial);
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+  }
 }
+
+// Available password criteria
+let lowercase = "abcdefghijklmnopqrstuvwxyz";
+let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let numerals = "0123456789";
+let special = "!\"#$%&'()*+,-./:;<=>?@[\]\\^_`{|}~";

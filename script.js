33e7369b1d566b547acb79388c094c18e550d550
jabrams513 <145389973+jabrams513@users.noreply.github.com>
2023-11-26 13:28:26 -0500

    added comments, gave strings for selectable characters and validated them. iterate up to promptedLength  value.

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
+// Ask for password length and validate if the length falls within the range of the prompt
 function writePassword() {
   let promptedLength;
   let validLength = false;
@@ -12,24 +13,61 @@ function writePassword() {
     promptedLength = prompt("How many characters would you like in your password? You may have between 8 and 128 characters.");
     if (promptedLength >= 8 && promptedLength <= 128) {
       validLength = true;
-    } else if (promptLength === null) {
-      break;
+    } else if (promptedLength === false) {
+      // break;
     }
   }
 
+  // If length is in valid range, ask a series of questions regarding which character types to include
   if (validLength == true) {
     let useLowercase = confirm("Use lowercase characters? 'Ok' for yes, 'Cancel' for no.");
     let useUppercase = confirm("Use uppercase characters? 'Ok' for yes, 'Cancel' for no.");
     let useNumerals = confirm("Use numerals characters? 'Ok' for yes, 'Cancel' for no.");
     let useSpecial = confirm("Use special characters? 'Ok' for yes, 'Cancel' for no.");
-    var password = generatePassword(promptLength, useLowercase, useUppercase, useNumerals, useSpecial);
+    var password = generatePassword(promptedLength, useLowercase, useUppercase, useNumerals, useSpecial);
     var passwordText = document.querySelector("#password");
     passwordText.value = password;
   }
 }
 
-// Available password criteria
-let lowercase = "abcdefghijklmnopqrstuvwxyz";
-let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let numerals = "0123456789";
-let special = "!\"#$%&'()*+,-./:;<=>?@[\]\\^_`{|}~";
+// Define strings available for use in character sets
+const generatePassword = (promptedLength, lowercase, uppercase, numeric, special) => {
+  const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numericChars = "0123456789";
+  const specialChars = "!\"#$%&'()*+,-./:;<=>?@[\]\\^_`{|}~";
+
+  // Compile a list of selected character types
+  let selectedCharTypes = "";
+
+  if (lowercase) {
+    selectedCharTypes += lowercaseChars
+  }
+
+  if (uppercase) {
+    selectedCharTypes += uppercaseChars
+  }
+
+  if (numeric) {
+    selectedCharTypes += numericChars
+  }
+
+  if (special) {
+    selectedCharTypes += specialChars
+  }
+
+  // Alert if no types of characters are selected
+  if (selectedCharTypes == "") {
+    console.log()
+    alert("You must select some type of characters to generate a password");
+  }
+  // Randomly select characters until reaching the prompted length
+  else {
+    let Password = ""
+    for (let i = 0; i <= promptedLength; i++) {
+      let randomIndex = Math.floor(Math.random() * selectedCharTypes.length);
+      Password += selectedCharTypes.substring(randomIndex, randomIndex + 1);
+    }
+    return Password;
+  }
+}
